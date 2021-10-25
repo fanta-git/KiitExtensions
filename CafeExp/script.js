2d993363404ac2194e6a92c37f5a5b279f8fe148
fanta-git <fantaji4957t@gmail.com>
2021-10-25 20:26:10 +0900

    文量を削った 関数だったのをばらした brankの表示の仕方を変えた とか

--- CafeExp/script.js
+++ CafeExp/script.js
@@ -1,4 +1,6 @@
-let timetableItemTemplate = $(`
+'use strict';
+
+const timetableItemTemplate = $(`
 	<li data-timestamp="">
 		<div class="bg_black"></div>
 		<div class="thumbnail" style=""></div>
@@ -17,11 +19,19 @@ let timetableItemTemplate = $(`
 	</li>
 `).get(0);
 
-let timetableCommentTemplate = $(`
+const timetableCommentTemplate = $(`
 	<div class="comment">
 		<div class="comment_icon" style=""></div>
 		<div class="comment_text"></div>
 	</div>
+`).get(0);
+
+const timetableBrankTemplate = $(`
+	<li>
+		<div class="timetable_brank">
+			<i class="material-icons">more_vert</i>
+		</div>
+	</li>
 `).get(0);
 
 let timetableDic = JSON.parse(localStorage.timetable || '[]');
@@ -55,7 +65,7 @@ $(() => {
 				</div>
 				<div id="music_description">
 					動画を再生すると説明文や再生数などがここに表示されます<br>
-					曲名の右にある人のマークを押すと普段の画面になります
+					再生しても表示されない時は再読み込みしてください
 				</div>
 			</div>
 		</div>
@@ -76,25 +86,27 @@ $(() => {
 			<div class="inner">
 				<h2>選曲履歴</h2>
 				<div class="exp">
-					これまで流れた曲とコメントを確認することができます<br>
+					これまで流れた曲とコメントを最大100曲分まで確認することができます<br>
 					あなたがKiiteCafeを開いていない時の履歴を確認することはできません
 				</div>
 				<div id="timetable_del_wrapper">
 					<div id="timetable_del">履歴を全て削除</div>
 				</div>
 				<div id="timetable_list">
+					<ul></ul>
 				</div>
 			</div>
 		</div>
 	`);
 
-	const timetableListNode = $('<ul></ul>').get(0);
+	let timetableListNode = document.createDocumentFragment();
 	for(const element of timetableDic){
-		timetableListNode.append(timetableItemCreate(element));
+		timetableListNode.appendChild(timetableItemCreate(element));
 	}
-	$('#timetable_list').append($(timetableListNode));
+	document.querySelector('#timetable_list > ul').appendChild(timetableListNode);
 	
-	document.querySelector('#cafe').classList.add('view_reasons');
+	const qsCafe = document.querySelector('#cafe');
+	qsCafe.classList.add('view_reasons');
 
 	let obsData = {};
 	setInterval(() => {
@@ -105,10 +117,11 @@ $(() => {
 				userId: element.dataset.user_id,
 				userName: element.querySelector('.user_nickname').textContent
 			};
-			if(!!commentData.text && obsData[commentData.userId] !== commentData.text){  //コメントを記録
+			if(obsData[commentData.Id] !== null && !!commentData.text && obsData[commentData.userId] !== commentData.text){
 				if(localStorage.ntc_flag === 'true' && !document.hasFocus()){
-					Notification.requestPermission();
-					var notification = new Notification(commentData.text,{ body : commentData.userName });
+					Notification.requestPermission().then(permission => {
+						new Notification(commentData.text,{ body : commentData.userName });
+					});
 				}
 				$('#timetable_list > ul > li:first-child > .comment_list').append($(timetableCommentCreate(commentData)));
 				timetableDic[0].commentList.push(commentData);
@@ -119,32 +132,35 @@ $(() => {
 		};
 	}, 1000);
 
-	const viewclass = Array.from(document.querySelectorAll('#cafe_menu > ul > li'), v => `view_${v.className}`);
-	for(const element of document.querySelectorAll('#cafe_menu > ul > li')){
-		$(document).on('click', `#cafe_menu > ul > li.${element.className}`, () => {
-			document.querySelector('#cafe').classList.remove(...viewclass);
-			document.querySelector('#cafe').classList.add(`view_${element.className}`);
+	const qsaMenuLi = document.querySelectorAll('#cafe_menu > ul > li'),
+		viewclass = Array.from(qsaMenuLi, v => `view_${v.dataset.val}`);
+	for(const element of qsaMenuLi){
+		$(document).on('click', `#cafe_menu > ul > li.${element.dataset.val}`, () => {
+			qsCafe.classList.remove(...viewclass);
+			qsCafe.classList.add(`view_${element.dataset.val}`);
 		});
 	};
 });
 
 $(document).on("click", "#rd_toggle", () => {
-	const element = document.querySelector('#rd_toggle .material-icons');
-	if(element.innerText === 'info'){
-		element.innerText = 'people';
-		document.querySelector('#cafe').classList.remove('view_reasons');
-		document.querySelector('#cafe').classList.add('view_music_data');
+	const qsRdIcon = document.querySelector('#rd_toggle .material-icons'),
+		qsCafe = document.querySelector('#cafe');
+	
+	if(qsRdIcon.innerText === 'info'){
+		qsRdIcon.innerText = 'people';
+		qsCafe.classList.remove('view_reasons');
+		qsCafe.classList.add('view_music_data');
 	}else{
-		element.innerText = 'info';
-		document.querySelector('#cafe').classList.remove('view_music_data');
-		document.querySelector('#cafe').classList.add('view_reasons');
+		qsRdIcon.innerText = 'info';
+		qsCafe.classList.remove('view_music_data');
+		qsCafe.classList.add('view_reasons');
 	}
 });
 
 $(document).on("click", "#ntc_toggle", () => {
 	Notification.requestPermission();
 	localStorage.ntc_flag = localStorage.ntc_flag === 'true' ? 'false' : 'true';
-	document.querySelector('#ntc_toggle .material-icons').innerText = localStorage.ntc_flag === 'true'? 'notifications_active' : 'notifications_off';
+	document.querySelector('#ntc_toggle .material-icons').innerText = (localStorage.ntc_flag === 'true'? 'notifications_active' : 'notifications_off');
 });
 
 $(document).on("click", "#timetable_del", () => {
@@ -161,15 +177,9 @@ chrome.runtime.onMessage.addListener((request) => {
 	if(request.type === 'music_data'){
 		const music_data = JSON.parse(request.data);
 		console.log(music_data);
-		document.querySelector('#viewCounter').innerText = (
-			parseInt(music_data.viewCounter).toLocaleString()
-		);
-		document.querySelector('#mylistCounter').innerText = (
-			parseInt(music_data.mylistCounter).toLocaleString()
-		);
-		document.querySelector('#commentCounter').innerText = (
-			parseInt(music_data.thread.commentCounter).toLocaleString()
-		);
+		document.querySelector('#viewCounter').innerText = parseInt(music_data.viewCounter).toLocaleString();
+		document.querySelector('#mylistCounter').innerText = parseInt(music_data.mylistCounter).toLocaleString();
+		document.querySelector('#commentCounter').innerText = parseInt(music_data.thread.commentCounter).toLocaleString();
 		document.querySelector('#music_description').innerHTML = (
 			music_data.description
 				.replace(/https?:\/\/[\w!?/+\-~=;.,*&@#$%()'[\]]+/g, '<a href="$&" target="_blank">$&</a>')
@@ -177,61 +187,60 @@ chrome.runtime.onMessage.addListener((request) => {
 				.replace(/(?<!(https?:\/\/[\w!?/+\-~=;.,*&@#$%()'[\]]+|@\w+))(sm|nm)\d+/g, '<a href="https://www.nicovideo.jp/watch/$&" target="_blank">$&</a>')
 				.replace(/(?<!(https?:\/\/[\w!?/+\-~=;.,*&@#$%()'[\]]+|\w+))@(\w+)/g, '<a href="https://twitter.com/$1" target="_blank">$&</a>')
 				.replace(/#[0-9a-fA-F]{6}/g, ((match) => {
-					let befCol = {};
-					befCol = {
-						r: parseInt(match.substr(1, 2), 16),
-						g: parseInt(match.substr(3, 2), 16),
-						b: parseInt(match.substr(5, 2), 16)
+					let r, g, b;
+					[r, g, b] = [parseInt(match.substr(1, 2), 16), parseInt(match.substr(3, 2), 16), parseInt(match.substr(5, 2), 16)];
+					const thr = 6;
+
+					const blightRatio = (_r, _g, _b) => {
+						[_r, _g, _b] = [Math.min(_r / 255, 1), Math.min(_g / 255, 1), Math.min(_b / 255, 1)];
+
+						return (
+							(_r <= .3298 ? _r / 12.92 : ((_r + .055) / 1.055) ** 2.4) * .2126 +
+							(_g <= .3298 ? _g / 12.92 : ((_g + .055) / 1.055) ** 2.4) * .7512 +
+							(_b <= .3298 ? _b / 12.92 : ((_b + .055) / 1.055) ** 2.4) * .0722 + 
+							.05
+						)/ .05
 					};
-					console.log(befCol);
-					const aftcol = rgbToHex(changeColor(befCol, 6));
-					return aftcol;
+
+					if(r === g && g === b){
+						[r, g, b] = [255 - r, 255 - g, 255 - b];
+					}else if(blightRatio(r, g, b) < thr){
+						[r, g, b] = [r || 1, g || 1, b || 1];
+
+						let top = 255 / Math.min(r, g, b), 
+							bottom = 1,
+							mag = (top + bottom)/ 2;
+
+						for(let i = 0; i < 8; i++){
+							if(blightRatio(r * mag, g * mag, b * mag) < thr){
+								bottom = mag;
+							}else{
+								top = mag;
+							}
+							mag = (top + bottom)/ 2;
+						}
+						[r, g, b] = [Math.min(Math.round(r * mag), 255), Math.min(Math.round(g * mag), 255), Math.min(Math.round(b * mag), 255)]
+					}
+
+					return('#' + r.toString(16).padStart(2, '0') + g.toString(16).padStart(2, '0') + b.toString(16).padStart(2, '0'));
 				}))
 		);
 
 		setTimeout(() => {
-			const 
-				nowtime = Date.now(), 
+			const nowtime = Date.now(), 
 				loadedtime = new Date(parseInt(music_data.actionTrackId.split('_')[1])), 
 				song_position = new Date((1 - parseFloat(document.querySelector('#song_position .position').style.width.slice(0, -1))/ 100) * music_data.lengthInSeconds * 1000), 
 				timestamp_time = new Date(loadedtime.getTime() - song_position.getTime());
 
-			console.log('song_position', 1 - parseFloat(document.querySelector('#song_position .position').style.width.slice(0, -1))/ 100);
-
-			//loadedtime    : ユーザーが曲を再生し始めた時間(日付)
-			//song_position : 再生を始めてからの再生時間(時々ずれる、2秒くらい早い Date型だが中身は秒数なので文字列で表示すると変な感じになる)
-			//timestamp_time: Cafeで曲が再生されたであろう時間
-
-			//時系列                ||0---\\-----|===============|============|------->未来
-			//                     ||    //     |<-曲が再生開始   |<-Cafeに入る |<-流れ終わる
-			//loadedtime           || <--\\-----+-------------->|            +
-			//song_position        ||    //     |<------------->|            +
-			//timestamp_time       || <--\\---->|               +            +
-			//localStorage.endtime || <--//-----+---------------+----------->|(処理の最後に代入するので基本的には一つ前の曲からとった数字を扱う)
-
-			if(!!localStorage.endtime){
-				console.log(localStorage.endtime + 0, timestamp_time.getTime(), timestamp_time.getTime() - localStorage.endtime);
-				if(parseInt(localStorage.endtime) + 10000 < timestamp_time.getTime()){
-					$('#timetable_list > ul').prepend(`
-						<li>
-							<div class="timetable_brank">
-								<i class="material-icons">more_vert</i>
-							</div>
-						</li>
-					`);
-					timetableDic.unshift({blank: true});
-				}
-			}
-
-			const lasttitle = document.querySelector('#timetable_list .title');
-			if(!lasttitle || lasttitle.innerText !== music_data.title){
+			if(!timetableDic[0] || timetableDic[0].title !== music_data.title){
 				timetableDic.unshift({
 					"timestamp": timestamp_time.getTime(),
 					"thumbnailId": music_data.thumbnailUrl.split('/')[5],
 					"title": music_data.title,
 					"artist": document.querySelector('#now_playing_info .artist span').textContent,
 					"videoId": music_data.videoId,
-					"commentList": []
+					"commentList": [],
+					"brank": !!localStorage.endtime && parseInt(localStorage.endtime) + 10000 < timestamp_time.getTime()
 				});
 				if(100 < timetableDic.length){
 					timetableDic.splice(100, timetableDic.length - 100);
@@ -240,14 +249,15 @@ chrome.runtime.onMessage.addListener((request) => {
 				$('#timetable_list > ul').prepend($(timetableItemCreate(timetableDic[0])));
 
 				if(localStorage.ntc_flag === 'true' && !document.hasFocus()){
-					Notification.requestPermission();
-					var notification = new Notification(music_data.title);
+					Notification.requestPermission().then((permission) => {
+						new Notification(music_data.title);
+					});
 				}
 
 				localStorage.timetable = JSON.stringify(timetableDic);
 				localStorage.endtime = (timestamp_time.getTime() + (music_data.lengthInSeconds * 1000)) + '';
 			}
-			
+
 			document.querySelectorAll('#timetable_list > ul > li').forEach((element, index) => {
 				if(index < 100){
 					if(!!element.querySelectorAll('.timestamp').length){
@@ -271,14 +281,18 @@ chrome.runtime.onMessage.addListener((request) => {
 });
 
 function timetableItemCreate(itemData){
-	let newNode = timetableItemTemplate.cloneNode(true);
-	newNode.dataset.timestamp = itemData.timestamp;
+	let newNode = document.createDocumentFragment();
+	newNode.appendChild(timetableItemTemplate.cloneNode(true));
+	newNode.querySelector('li').dataset.timestamp = itemData.timestamp;
 	newNode.querySelector('.thumbnail').style.backgroundImage = `url('https://nicovideo.cdn.nimg.jp/thumbnails/${itemData.thumbnailId.split('.')[0]}/${itemData.thumbnailId}')`;
 	newNode.querySelector('.title').textContent = itemData.title;
 	newNode.querySelector('.artist').textContent = itemData.artist;
 	newNode.querySelector('.source > a').href = `https://kiite.jp/search/song?keyword=${itemData.videoId}`;
 	for(const element of itemData.commentList){
 		newNode.querySelector('.comment_list').appendChild(timetableCommentCreate(element));
+	}
+	if(itemData.brank){
+		newNode.appendChild(timetableBrankTemplate.cloneNode(true));
 	}
 	return newNode;
 }
@@ -288,59 +302,4 @@ function timetableCommentCreate(itemData){
 	newNode.querySelector('.comment_icon').style.backgroundImage = `url('https://d7209z8dzwjpy.cloudfront.net/avatar/${itemData.iconName}')`;
 	newNode.querySelector('.comment_text').textContent = itemData.text;
 	return newNode;
-}
-
-function changeColor(textRGB, thr = 7) {  //ニコニコは白背景、Kiiteは黒背景で文字色そのままに動画説明文を載せると読みにくいためそれを直す
-	let {r, g, b} = textRGB;
-	const blightRatio = (_r, _g, _b) => {  //#000000とのコントラストを返す関数 https://lifehackdev.com/ZakkiBlog/articles/detail/web15
-		_r = Math.min(_r / 255, 1);
-		_g = Math.min(_g / 255, 1);
-		_b = Math.min(_b / 255, 1);
-
-		return (
-			(_r <= .3298 ? _r / 12.92 : ((_r + .055) / 1.055) ** 2.4) * .2126 +
-			(_g <= .3298 ? _g / 12.92 : ((_g + .055) / 1.055) ** 2.4) * .7512 +
-			(_b <= .3298 ? _b / 12.92 : ((_b + .055) / 1.055) ** 2.4) * .0722 + 
-			.05
-		)/ .05
-	};
-
-	if(r === g && g === b){  //グレー系は反転だけして返す
-		return { r: 255 - r, g: 255 - g, b: 255 - b };
-	}
-
-	if(thr <= blightRatio(r, g, b)){  //コントラストが閾値以上ならそのまま返す
-		return { r: r, g: g, b: b };
-	}
-
-	r = r || 1;  //0なら1にする
-	g = g || 1;
-	b = b || 1;
-
-	let top = 255 / Math.min(r, g, b), bottom = 1, mag = (top + bottom)/ 2;
-
-	for(let i = 0; i < 8; i++){  //コントラストの逆算は怠いので二分探索のノリで出す
-		if(blightRatio(r * mag, g * mag, b * mag) < thr){
-			bottom = mag;
-		}else{
-			top = mag;
-		}
-		mag = (top + bottom)/ 2;
-	}
-
-	return {
-		r: Math.min(Math.round(r * mag), 255), 
-		g: Math.min(Math.round(g * mag), 255), 
-		b: Math.min(Math.round(b * mag), 255)
-	};
-}
-
-function rgbToHex(color){
-	const {r, g, b} = color;
-	return(
-		'#' + 
-		(r || 0).toString(16).padStart(2, '0') + 
-		(g || 0).toString(16).padStart(2, '0') + 
-		(b || 0).toString(16).padStart(2, '0')
-	);
 }
