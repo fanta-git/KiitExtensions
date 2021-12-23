3174b2f55045630c512c006e9386705767d52f8b
fanta-git <fantaji4957t@gmail.com>
2021-12-23 20:28:53 +0900

    マニフェストをv3に移行 background.jsなしで動くように 特別メニューの表示に対応 可読性を上げようとした

--- CafExtension/iframe.js
+++ CafExtension/iframe.js
@@ -1,7 +1,6 @@
 window.onload = () => {
 	const music_data = document.querySelector('#ext-player').dataset.props;
 	if(music_data && JSON.parse(music_data).parentUrl.startsWith('https://cafe.kiite.jp/')){
-		const send_data = {type: 'music_data', data: music_data};
-		chrome.runtime.sendMessage(send_data);
+		chrome.storage.local.set({music_data: JSON.parse(music_data)});
 	}
 };
--- CafExtension/script.js
+++ CafExtension/script.js
@@ -16,7 +16,7 @@ const timetableItemTemplate = new Range().createContextualFragment(`
 		<div class="thumbnail" style=""></div>
 		<div class="music_info">
 			<div class="onair">ON AIR</div>
-			<div class="timestamp">--分前</div>
+			<div class="timestamp">**分前</div>
 			<div class="reason">
 				<div class="icon" style=""></div>
 				<div class="text">
@@ -53,7 +53,7 @@ const timetableCommentTemplate = new Range().createContextualFragment(`
 	</div>
 `);
 
-const timetableBrankTemplate =  new Range().createContextualFragment(`
+const timetableBrankTemplate = new Range().createContextualFragment(`
 	<div class="timetable_item">
 		<div class="timetable_brank">
 			<i class="material-icons">more_vert</i>
@@ -73,11 +73,31 @@ const reasonTextPlaylistTemplate = new Range().createContextualFragment(`
 	<a class="user_name" href="" target="_blank"></a>さんの<b class="playlist">プレイリスト</b>の曲です
 `);
 
-const timetableDic = JSON.parse(localStorage.timetable ?? '[]');
+const reasonTextSpecialTemplate = new Range().createContextualFragment(`
+	<a class="user_name" href="" target="_blank"></a>さんの<b class="playlist">特別メニュー</b>の曲です
+`);
+
+const timetableDic = JSON.parse(localStorage.timetable ?? '[]'), 
+	obsComment = {};
 let musicEndtime = parseInt(localStorage.endtime),
 	notice_flag = localStorage.ntc_flag === 'true';
 
 window.onload = () => {
+	setMenuDom();
+
+	setTimeout(() => {
+		chrome.storage.onChanged.addListener((changes, namespace) => {
+			for (const [key, {oldValue, newValue}] of Object.entries(changes)) {
+				if(key === 'music_data' && newValue.videoId){
+					pushTimetable(newValue);
+				}
+			}
+		});
+		setInterval(observeUserCondition, settings.intervalTime)
+	}, settings.waitTime);
+};
+
+function setMenuDom(){
 	document.querySelector('#now_playing_info .source').insertAdjacentHTML('afterend', `
 		<div class="rd_toggle" id="rd_toggle">
 			<i class="material-icons">info</i>
@@ -144,6 +164,7 @@ window.onload = () => {
 			timetableListNode.append(timetableItemCreate(element));
 		}
 		document.querySelector('#timetable_list').append(timetableListNode);
+		updateTimecounter(Date.now());
 	}
 
 	const qsCafe = document.querySelector('#cafe'),
@@ -158,7 +179,8 @@ window.onload = () => {
 
 	document.querySelector('#ntc_toggle').onclick = () => {
 		Notification.requestPermission();
-		localStorage.ntc_flag = (notice_flag = !notice_flag) ? 'true' : 'false';
+		notice_flag = !notice_flag;
+		localStorage.ntc_flag = (notice_flag ? 'true' : 'false');
 		document.querySelector('#ntc_toggle .material-icons').textContent = (notice_flag ? 'notifications_active' : 'notifications_off');
 	};
 
@@ -173,205 +195,206 @@ window.onload = () => {
 		if(confirm('本当に再生履歴を全て削除しますか？')){
 			localStorage.timetable = '[]';
 			localStorage.endtime = '';
-			timetableDic = [];
+			timetableDic.length = 0;
 			musicEndtime = 0;
+			chrome.storage.local.remove('music_data');
 			for(const element of document.querySelectorAll('#timetable_list .timetable_item')){
 				element.remove();
 			};
 		}
 	};
+}
+
+function pushTimetable(music_data){
+	console.log(music_data);
+
+	if(notice_flag && !document.hasFocus()){
+		Notification.requestPermission().then(() => {
+			new Notification(music_data.title);
+		});
+	}
+
+	document.querySelector('#viewCounter').textContent = parseInt(music_data.viewCounter).toLocaleString();
+	document.querySelector('#mylistCounter').textContent = parseInt(music_data.mylistCounter).toLocaleString();
+	document.querySelector('#commentCounter').textContent = parseInt(music_data.thread.commentCounter).toLocaleString();
+	document.querySelector('#music_description').innerHTML = (
+		music_data.description
+			.replace(/https?:\/\/[\w!?/+\-~=;.,*&@#$%()'[\]]+/g, '<a href="$&" target="_blank">$&</a>')
+			.replace(/(?<![\/\w@])(mylist|user)\/\d+/g,'<a href="https://www.nicovideo.jp/$&" target="_blank">$&</a>')
+			.replace(/(?<![\/\w@])(sm|nm)\d+/g, '<a href="https://www.nicovideo.jp/watch/$&" target="_blank">$&</a>')
+			.replace(/(?<![\/\w@])nc\d+/g, '<a href="https://commons.nicovideo.jp/material/$&" target="_blank">$&</a>')
+			.replace(/(?<![\/\w@])co\d+/g, '<a href="https://com.nicovideo.jp/community/$&" target="_blank">$&</a>')
+			.replace(/(?<![\/\w])@(\w+)/g, '<a href="https://twitter.com/$1" target="_blank">$&</a>')
+			.replace(/#[0-9a-fA-F]{6}/g, ((match) => {
+				let [r, g, b] = [parseInt(match.substr(1, 2), 16), parseInt(match.substr(3, 2), 16), parseInt(match.substr(5, 2), 16)];
+				const blightRatio = (_r, _g, _b) => {
+					[_r, _g, _b] = [Math.min(_r / 255, 1), Math.min(_g / 255, 1), Math.min(_b / 255, 1)];
+
+					return (
+						(_r <= .3298 ? _r / 12.92 : ((_r + .055) / 1.055) ** 2.4) * .2126 +
+						(_g <= .3298 ? _g / 12.92 : ((_g + .055) / 1.055) ** 2.4) * .7512 +
+						(_b <= .3298 ? _b / 12.92 : ((_b + .055) / 1.055) ** 2.4) * .0722 + 
+						.05
+					)/ .05
+				};
+
+				if(r === g && g === b){
+					[r, g, b] = [255 - r, 255 - g, 255 - b];
+				}else if(blightRatio(r, g, b) < settings.colorThreshold){
+					[r, g, b] = [r || 1, g || 1, b || 1];
+					let top = 255 / Math.min(r, g, b), bottom = 1,mag = (top + bottom)/ 2;
+
+					for(let i = 0; i < 8; i++){
+						if(blightRatio(r * mag, g * mag, b * mag) < settings.colorThreshold){
+							bottom = mag;
+						}else{
+							top = mag;
+						}
+						mag = (top + bottom)/ 2;
+					}
+					[r, g, b] = [Math.min(Math.round(r * mag), 255), Math.min(Math.round(g * mag), 255), Math.min(Math.round(b * mag), 255)]
+				}
+
+				return('#' + r.toString(16).padStart(2, '0') + g.toString(16).padStart(2, '0') + b.toString(16).padStart(2, '0'));
+			}))
+	);
 
 	setTimeout(() => {
-		const obsComment = {};
-		setInterval(() => {
-			if(!!timetableDic.length){
-				if(Date.now() < musicEndtime + settings.endtimeTolerance){
-					const qsaUser = document.querySelectorAll('#cafe_space .user'),
-						qsTimetableFirst = document.querySelector('#timetable_list .timetable_item:first-child');
-					const newData = {};
-					qsaUser.forEach(e => {
-						e.classList.forEach(v => {
-							newData[v] = (newData[v] ?? 0) + 1;
-						});
-					});
-					for(const element of qsaUser){
-						const commentData = {
-							iconUrl: element.querySelector('.thumbnail').style.backgroundImage.split('"')[1],
-							text: element.querySelector('.comment').textContent,
-							userId: element.dataset.user_id,
-							userName: element.querySelector('.user_nickname').textContent, 
-							presenter: element.classList.contains('presenter'), 
-						};
-						if(obsComment[commentData.userId] !== undefined && !!commentData.text && obsComment[commentData.userId] !== commentData.text){
-							if(notice_flag && !document.hasFocus()){
-								Notification.requestPermission().then(() => {
-									new Notification(commentData.text,{ body : commentData.userName });
-								});
-							}
-							qsTimetableFirst.querySelector('.comment_list').append(timetableCommentCreate(commentData));
-							qsTimetableFirst.querySelector('.comment_list').classList.remove('empty');
-							timetableDic[0].commentList.push(commentData);
-							localStorage.timetable = JSON.stringify(timetableDic);
-						}
-						obsComment[commentData.userId] = commentData.text;
-					}
-					if(timetableDic[0].gesture_rotate < newData?.gesture_rotate){
-						if(!timetableDic[0].gesture_rotate){
-							qsTimetableFirst.querySelector('.rotate').classList.remove('invisible');
-						}
-						qsTimetableFirst.querySelector('.rotate > .count').textContent = newData.gesture_rotate;
-						timetableDic[0].gesture_rotate = newData.gesture_rotate;
-					}
-					if(timetableDic[0].new_fav < newData?.new_fav){
-						if(!timetableDic[0].new_fav){
-							qsTimetableFirst.querySelector('.new_fav').classList.remove('invisible');
-						}
-						qsTimetableFirst.querySelector('.new_fav > .count').textContent = newData.new_fav;
-						timetableDic[0].new_fav = newData.new_fav;
-					}
-				}else{
-					document.querySelectorAll('#timetable_list .timetable_item.onair_now').forEach(e => {
-						e.classList.remove('onair_now');
-					});
-				}
-			}
-		}, settings.intervalTime);
-	}, settings.waitTime);
-};
+		const nowtime = Date.now(), 
+			loadedtime = parseInt(music_data.actionTrackId.split('_')[1]), 
+			song_position = (1 - parseFloat(document.querySelector('#song_position .position').style.width.slice(0, -1))/ 100) * music_data.lengthInSeconds * 1000, 
+			timestamp_time = loadedtime - song_position,
+			qsReasonFirst = document.querySelector('#reasons li:first-child');
 
-chrome.runtime.onMessage.addListener((request) => {
-	if(request.type === 'music_data'){
-		const music_data = JSON.parse(request.data);
-		console.log(music_data);
-
-		if(notice_flag && !document.hasFocus()){
-			Notification.requestPermission().then(() => {
-				new Notification(music_data.title);
+		if(timetableDic[0]?.title !== music_data.title){
+			timetableDic.unshift({
+				timestamp: timestamp_time,
+				thumbnailUrl: music_data.thumbnailUrl,
+				title: music_data.title,
+				artist: document.querySelector('#now_playing_info .artist span').textContent,
+				videoId: music_data.videoId,
+				gesture_rotate: 0,
+				new_fav: 0,
+				reason: {
+					iconUrl: qsReasonFirst.querySelector('.user_icon').style.backgroundImage.split('"')[1],
+					userNickname: qsReasonFirst.querySelector('.text a:nth-child(1)').textContent,
+					userUrlId: qsReasonFirst.querySelector('.text a:nth-child(1)').href.split('/')[4],
+					listUrl: '',
+					text: Array.from(qsReasonFirst.querySelectorAll('.comment span'), e => e.textContent),
+				}, 
+				commentList: [],
+				brank: !!musicEndtime && musicEndtime + settings.endtimeTolerance < timestamp_time,
 			});
+
+			const qsReasonKind = qsReasonFirst.querySelector('.text a:nth-child(2), .text b');
+			switch(qsReasonKind.textContent){
+				case 'イチ推しリスト':
+					timetableDic[0].reason.listUrl = qsReasonKind.href.split('/')[4];
+					break;
+				case 'お気に入り':
+					timetableDic[0].reason.listUrl = '__faves__';
+					break;
+				case 'プレイリスト':
+					timetableDic[0].reason.listUrl = '__playlist__';
+					break;
+				case '特別メニュー':
+					timetableDic[0].reason.listUrl = '__specialmenu__';
+			}
+
+			if(settings.timetableMax < timetableDic.length){
+				timetableDic.splice(settings.timetableMax);
+			}
+
+			document.querySelector('#timetable_list').prepend(timetableItemCreate(timetableDic[0]));
+
+			localStorage.timetable = JSON.stringify(timetableDic);
+			musicEndtime = timestamp_time + music_data.lengthInSeconds * 1000;
+			localStorage.endtime = musicEndtime + '';
 		}
 
-		document.querySelector('#viewCounter').textContent = parseInt(music_data.viewCounter).toLocaleString();
-		document.querySelector('#mylistCounter').textContent = parseInt(music_data.mylistCounter).toLocaleString();
-		document.querySelector('#commentCounter').textContent = parseInt(music_data.thread.commentCounter).toLocaleString();
-		document.querySelector('#music_description').innerHTML = (
-			music_data.description
-				.replace(/https?:\/\/[\w!?/+\-~=;.,*&@#$%()'[\]]+/g, '<a href="$&" target="_blank">$&</a>')
-				.replace(/(?<![\/\w@])(mylist|user)\/\d+/g,'<a href="https://www.nicovideo.jp/$&" target="_blank">$&</a>')
-				.replace(/(?<![\/\w@])(sm|nm)\d+/g, '<a href="https://www.nicovideo.jp/watch/$&" target="_blank">$&</a>')
-				.replace(/(?<![\/\w@])nc\d+/g, '<a href="https://commons.nicovideo.jp/material/$&" target="_blank">$&</a>')
-				.replace(/(?<![\/\w@])co\d+/g, '<a href="https://com.nicovideo.jp/community/$&" target="_blank">$&</a>')
-				.replace(/(?<![\/\w])@(\w+)/g, '<a href="https://twitter.com/$1" target="_blank">$&</a>')
-				.replace(/#[0-9a-fA-F]{6}/g, ((match) => {
-					let [r, g, b] = [parseInt(match.substr(1, 2), 16), parseInt(match.substr(3, 2), 16), parseInt(match.substr(5, 2), 16)];
-					const blightRatio = (_r, _g, _b) => {
-						[_r, _g, _b] = [Math.min(_r / 255, 1), Math.min(_g / 255, 1), Math.min(_b / 255, 1)];
+		updateTimecounter(nowtime);
+	}, settings.waitTime);
+}
 
-						return (
-							(_r <= .3298 ? _r / 12.92 : ((_r + .055) / 1.055) ** 2.4) * .2126 +
-							(_g <= .3298 ? _g / 12.92 : ((_g + .055) / 1.055) ** 2.4) * .7512 +
-							(_b <= .3298 ? _b / 12.92 : ((_b + .055) / 1.055) ** 2.4) * .0722 + 
-							.05
-						)/ .05
-					};
-
-					if(r === g && g === b){
-						[r, g, b] = [255 - r, 255 - g, 255 - b];
-					}else if(blightRatio(r, g, b) < settings.colorThreshold){
-						[r, g, b] = [r || 1, g || 1, b || 1];
-						let top = 255 / Math.min(r, g, b), bottom = 1,mag = (top + bottom)/ 2;
-
-						for(let i = 0; i < 8; i++){
-							if(blightRatio(r * mag, g * mag, b * mag) < settings.colorThreshold){
-								bottom = mag;
-							}else{
-								top = mag;
-							}
-							mag = (top + bottom)/ 2;
-						}
-						[r, g, b] = [Math.min(Math.round(r * mag), 255), Math.min(Math.round(g * mag), 255), Math.min(Math.round(b * mag), 255)]
-					}
-
-					return('#' + r.toString(16).padStart(2, '0') + g.toString(16).padStart(2, '0') + b.toString(16).padStart(2, '0'));
-				}))
-		);
-
-		setTimeout(() => {
-			const nowtime = Date.now(), 
-				loadedtime = parseInt(music_data.actionTrackId.split('_')[1]), 
-				song_position = (1 - parseFloat(document.querySelector('#song_position .position').style.width.slice(0, -1))/ 100) * music_data.lengthInSeconds * 1000, 
-				timestamp_time = loadedtime - song_position,
-				qsReasonFirst = document.querySelector('#reasons li:first-child');
-
-			if(timetableDic[0]?.title !== music_data.title){
-				timetableDic.unshift({
-					timestamp: timestamp_time,
-					thumbnailUrl: music_data.thumbnailUrl,
-					title: music_data.title,
-					artist: document.querySelector('#now_playing_info .artist span').textContent,
-					videoId: music_data.videoId,
-					gesture_rotate: 0,
-					new_fav: 0,
-					reason: {
-						iconUrl: qsReasonFirst.querySelector('.user_icon').style.backgroundImage.split('"')[1],
-						userNickname: qsReasonFirst.querySelector('.text a:nth-child(1)').textContent,
-						userUrlId: qsReasonFirst.querySelector('.text a:nth-child(1)').href.split('/')[4],
-						listUrl: '',
-						text: Array.from(qsReasonFirst.querySelectorAll('.comment span'), e => e.textContent),
-					}, 
-					commentList: [],
-					brank: !!musicEndtime && musicEndtime + settings.endtimeTolerance < timestamp_time,
+function observeUserCondition(){
+	if(!!timetableDic.length){
+		if(Date.now() < musicEndtime + settings.endtimeTolerance){
+			const qsaUser = document.querySelectorAll('#cafe_space .user'),
+				qsTimetableFirst = document.querySelector('#timetable_list .timetable_item:first-child');
+			const newData = {};
+			qsaUser.forEach(e => {
+				e.classList.forEach(v => {
+					newData[v] = (newData[v] ?? 0) + 1;
 				});
-
-				const qsReasonKind = qsReasonFirst.querySelector('.text a:nth-child(2), .text b');
-				switch(qsReasonKind.textContent){
-					case 'イチ推しリスト':
-						timetableDic[0].reason.listUrl = qsReasonKind.href.split('/')[4];
-						break;
-					case 'お気に入り':
-						timetableDic[0].reason.listUrl = '__faves__';
-						break;
-					case 'プレイリスト':
-						timetableDic[0].reason.listUrl = '__playlist__';
-						break;
-				}
-
-				if(settings.timetableMax < timetableDic.length){
-					timetableDic.splice(settings.timetableMax);
-				}
-
-				document.querySelector('#timetable_list').prepend(timetableItemCreate(timetableDic[0]));
-
-				localStorage.timetable = JSON.stringify(timetableDic);
-				musicEndtime = timestamp_time + music_data.lengthInSeconds * 1000;
-				localStorage.endtime = musicEndtime + '';
-			}
-
-			document.querySelectorAll('#timetable_list .timetable_item').forEach((element, index) => {
-				if(index < settings.timetableMax){
-					if(!!index){
-						element.classList.remove('onair_now');
-					}else{
-						element.classList.add('onair_now')
-					}
-					if(!!element.querySelector('.timestamp')){
-						element.querySelector('.timestamp').textContent = ((lag) => {
-							if(lag < 60){
-								return ~~(lag) + '秒前';
-							}else if(lag < 3600){
-								return ~~(lag / 60) + '分前';
-							}else if(lag < 86400){
-								return ~~(lag / 3600) + '時間前';
-							}
-							return ~~(lag / 86400) + '日前';
-						})((nowtime - element.dataset.timestamp) / 1000);
-					}
-				}else{
-					element.remove();
-				}
 			});
-		}, settings.waitTime);
+			for(const element of qsaUser){
+				const commentData = {
+					iconUrl: element.querySelector('.thumbnail').style.backgroundImage.split('"')[1],
+					text: element.querySelector('.comment').textContent,
+					userId: element.dataset.user_id,
+					userName: element.querySelector('.user_nickname').textContent, 
+					presenter: element.classList.contains('presenter'), 
+				};
+				if(obsComment[commentData.userId] !== undefined && !!commentData.text && obsComment[commentData.userId] !== commentData.text){
+					if(notice_flag && !document.hasFocus()){
+						Notification.requestPermission().then(() => {
+							new Notification(commentData.text,{ body : commentData.userName });
+						});
+					}
+					qsTimetableFirst.querySelector('.comment_list').append(timetableCommentCreate(commentData));
+					qsTimetableFirst.querySelector('.comment_list').classList.remove('empty');
+					timetableDic[0].commentList.push(commentData);
+					localStorage.timetable = JSON.stringify(timetableDic);
+				}
+				obsComment[commentData.userId] = commentData.text;
+			}
+			if(timetableDic[0].gesture_rotate < newData?.gesture_rotate){
+				if(!timetableDic[0].gesture_rotate){
+					qsTimetableFirst.querySelector('.rotate').classList.remove('invisible');
+				}
+				qsTimetableFirst.querySelector('.rotate > .count').textContent = newData.gesture_rotate;
+				timetableDic[0].gesture_rotate = newData.gesture_rotate;
+			}
+			if(timetableDic[0].new_fav < newData?.new_fav){
+				if(!timetableDic[0].new_fav){
+					qsTimetableFirst.querySelector('.new_fav').classList.remove('invisible');
+				}
+				qsTimetableFirst.querySelector('.new_fav > .count').textContent = newData.new_fav;
+				timetableDic[0].new_fav = newData.new_fav;
+			}
+		}else{
+			document.querySelectorAll('#timetable_list .timetable_item.onair_now').forEach(e => {
+				e.classList.remove('onair_now');
+			});
+		}
 	}
-});
+}
+
+function updateTimecounter(nowtime){
+	document.querySelectorAll('#timetable_list .timetable_item').forEach((element, index) => {
+		if(index < settings.timetableMax){
+			if(!!index){
+				element.classList.remove('onair_now');
+			}else{
+				element.classList.add('onair_now')
+			}
+			if(!!element.querySelector('.timestamp')){
+				element.querySelector('.timestamp').textContent = ((lag) => {
+					if(lag < 60){
+						return ~~(lag) + '秒前';
+					}else if(lag < 3600){
+						return ~~(lag / 60) + '分前';
+					}else if(lag < 86400){
+						return ~~(lag / 3600) + '時間前';
+					}
+					return ~~(lag / 86400) + '日前';
+				})((nowtime - element.dataset.timestamp) / 1000);
+			}
+		}else{
+			element.remove();
+		}
+	});
+}
 
 function timetableItemCreate(itemData){
 	const newNode = document.createDocumentFragment();
@@ -387,6 +410,12 @@ function timetableItemCreate(itemData){
 			break;
 		case '__playlist__':
 			newNode.querySelector('.reason .text').append(reasonTextPlaylistTemplate.cloneNode(true));
+			if(!settings.displayReasonAll){
+				newNode.querySelector('.reason').classList.add('invisible');
+			}
+			break;
+		case '__specialmenu__':
+			newNode.querySelector('.reason .text').append(reasonTextSpecialTemplate.cloneNode(true));
 			if(!settings.displayReasonAll){
 				newNode.querySelector('.reason').classList.add('invisible');
 			}
