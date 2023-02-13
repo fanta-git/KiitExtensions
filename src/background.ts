b7d21bb2170401f70f2ca58231d314dc9c7d90c0
fanta-git <fantaji4957t@gmail.com>
2023-02-13 19:20:52 +0900

    キーボードショートカット機能を追加

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,5 @@
 import chromeStorage from "./util/chromeStorage";
 
-chrome.commands.onCommand.addListener(async command => {
-    console.log(`Command: ${command}`);
-    await chromeStorage.set('command', command);
+chrome.commands.onCommand.addListener(command => {
+    chromeStorage.set('command', command);
 })
