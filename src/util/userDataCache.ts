04513a454d8c044acf66f700ac403fc84c39089f
fanta-git <fantaji4957t@gmail.com>
2023-02-26 19:14:34 +0900

    ミス修正

--- src/util/userDataCache.ts
+++ src/util/userDataCache.ts
@@ -1,15 +1,14 @@
 import { User } from "./apiTypes";
 import fetchCafeAPI from "./fetchCafeAPI";
 
-
 export const userDataCache = new Map<number, User>();
 
 export async function fetchUserData(userIds: number[]) {
-    const omited = userIds
-        .filter((v, i, a) => a.indexOf(v) === i)
-        .filter(v => !userDataCache.has(v));
+    const userIdsSet = new Set(userIds);
+    userDataCache.forEach((_, id) => userIdsSet.delete(id));
+    if (userIdsSet.size) return;
 
-    const users = await fetchCafeAPI('/api/kiite_users', { user_ids: omited });
+    const users = await fetchCafeAPI('/api/kiite_users', { user_ids: [...userIdsSet] });
     for (const user of users) userDataCache.set(user.user_id, user);
 }
 
